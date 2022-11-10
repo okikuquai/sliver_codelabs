40903dc9dde7396a7ecd6ef283d2f40ed067c84c
Yuki Kikuchi <yuki-kikuchi@mail.nissan.co.jp>
2022-11-10 10:34:01 +0900

    step4 and step5実装

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,22 @@ class HorizonsApp extends StatelessWidget {
       scrollBehavior: const ConstantScrollBehavior(),
       title: 'Horizons Weather',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Horizons'),
-          backgroundColor: Colors.teal[800],
+        body: CustomScrollView(
+          slivers: <Widget>[
+            //sliverappbarでくくることで、ヘッダー要素をスクロールに応じて隠すことができる
+            SliverAppBar(
+              //pinnedをtrueにすることでヘッダーを完全に隠さなくする｀
+              pinned: true,
+              //floatingをtrueにすることで上方向にスクロールした際に場所に関係なくappbarの大きさが変わる
+              // floating: true,
+              // snap: true,
+              title: const Text('Horizons'),
+              backgroundColor: Colors.teal[800],
+              expandedHeight: 200.0,
+            ),
+            WeeklyForecastList(),
+          ],
         ),
-        body: const WeeklyForecastList(),
       ),
     );
   }
@@ -41,74 +52,75 @@ class WeeklyForecastList extends StatelessWidget {
     final DateTime currentDate = DateTime.now();
     final TextTheme textTheme = Theme.of(context).textTheme;
 
-    //listview
-    return ListView.builder(
-      itemCount: 7,
-      itemBuilder: (context, index) {
-        final DailyForecast dailyForecast = Server.getDailyForecastByID(index);
-        return Card(
-          child: Row(
-            children: <Widget>[
-              //画像
-              SizedBox(
-                height: 200.0,
-                width: 200.0,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    DecoratedBox(
-                      position: DecorationPosition.foreground,
-                      decoration: BoxDecoration(
-                        gradient: RadialGradient(
-                          colors: <Color>[
-                            Colors.grey[800]!,
-                            Colors.transparent
-                          ],
+    //sliverappbarを実装するために、ListView→SliverListに変更
+    return SliverList(
+      //処理を渡す？イメージ(delegate)
+      delegate: SliverChildBuilderDelegate(
+            (context, index) {
+          final DailyForecast dailyForecast =
+          Server.getDailyForecastByID(index);
+          return Card(
+            child: Row(
+              children: <Widget>[
+                SizedBox(
+                  height: 200.0,
+                  width: 200.0,
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      DecoratedBox(
+                        position: DecorationPosition.foreground,
+                        decoration: BoxDecoration(
+                          gradient: RadialGradient(
+                            colors: <Color>[
+                              Colors.grey[800]!,
+                              Colors.transparent
+                            ],
+                          ),
+                        ),
+                        child: Image.network(
+                          dailyForecast.imageId,
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      child: Image.network(
-                        dailyForecast.imageId,
-                        fit: BoxFit.cover,
+                      Center(
+                        child: Text(
+                          dailyForecast.getDate(currentDate.day).toString(),
+                          style: textTheme.headline2,
+                        ),
                       ),
-                    ),
-                    Center(
-                      child: Text(
-                        dailyForecast.getDate(currentDate.day).toString(),
-                        style: textTheme.headline2,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              //曜日とサブタイトル
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        dailyForecast.getWeekday(currentDate.weekday),
-                        style: textTheme.headline4,
-                      ),
-                      const SizedBox(height: 10.0),
-                      Text(dailyForecast.description),
                     ],
                   ),
                 ),
-              ),
-              //気温
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  '${dailyForecast.highTemp} | ${dailyForecast.lowTemp} F',
-                  style: textTheme.subtitle1,
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          dailyForecast.getWeekday(currentDate.weekday),
+                          style: textTheme.headline4,
+                        ),
+                        const SizedBox(height: 10.0),
+                        Text(dailyForecast.description),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    '${dailyForecast.highTemp} | ${dailyForecast.lowTemp} F',
+                    style: textTheme.subtitle1,
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+        childCount: 7,
+      ),
     );
   }
 }
@@ -231,7 +243,7 @@ class ConstantScrollBehavior extends ScrollBehavior {
       child;
 
   @override
-  TargetPlatform getPlatform(BuildContext context) => TargetPlatform.android;
+  TargetPlatform getPlatform(BuildContext context) => TargetPlatform.macOS;
 
   @override
   ScrollPhysics getScrollPhysics(BuildContext context) =>
